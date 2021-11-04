07bb64d7d344dd8e79f9b8c461a00f596e123098
safonov <safonov1985@protonmail.com>
2021-11-04 22:44:12 +0100

    First commit

---
+++ FloydSteinbergDithering/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FloydSteinbergDithering
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BitmapImage bitmap = new BitmapImage();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ImageLeft.AllowDrop = true;
+            
+        }
+
+        private void ImageLeft_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void MainGrid_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.All;
+        }
+
+        private void MainGrid_Drop(object sender, DragEventArgs e)
+        {
+            bitmap = new BitmapImage();
+            bitmap.BeginInit();
+
+            string[] fileList = e.Data.GetData(DataFormats.FileDrop) as string[];
+
+            bitmap.UriSource = new Uri(fileList[0]);
+            bitmap.EndInit();
+
+            ImageLeft.Source = bitmap;
+
+            ChangePicture();
+            
+        }
+
+        void ChangePicture()
+        {
+            DrawingVisual drawVis = new DrawingVisual();
+            using (DrawingContext dc = drawVis.RenderOpen())
+            {
+                dc.DrawImage(bitmap, new Rect(0, 0, bitmap.PixelWidth, bitmap.PixelHeight));
+                dc.DrawLine(new Pen(Brushes.Red, 2), new Point(0, 0), new Point(bitmap.Width, bitmap.Height));
+                dc.DrawRectangle(Brushes.Green, null, new Rect(20, 20, 150, 100));
+            }
+
+            RenderTargetBitmap rtb = new RenderTargetBitmap(bitmap.PixelWidth, bitmap.PixelHeight, 96, 96, PixelFormats.Pbgra32);
+            rtb.Render(drawVis);
+
+
+            ImageRight.Source = rtb;
+        }
+        
+    }
+}

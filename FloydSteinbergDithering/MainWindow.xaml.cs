b9187ea3f80a5d9fb3b5fbb701091a90caba67c3
safonov <safonov1985@protonmail.com>
2021-11-15 17:54:46 +0100

    Making pixels black or white, now we're getting somewhere

--- FloydSteinbergDithering/MainWindow.xaml.cs
+++ FloydSteinbergDithering/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,7 +27,7 @@ namespace FloydSteinbergDithering
         {
             InitializeComponent();
             ImageLeft.AllowDrop = true;
-            
+            //Debug.WriteLine(AverageNum(10, 50, 7));
         }
 
         private void ImageLeft_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -60,7 +61,8 @@ namespace FloydSteinbergDithering
         void ChangePicture()
         {
             //ImageRight.Source = Invert((BitmapSource)ImageLeft.Source);
-            ImageRight.Source = MakeGray((BitmapSource)ImageLeft.Source);
+            //ImageRight.Source = MakeGray((BitmapSource)ImageLeft.Source);
+            ImageRight.Source = BlackOrWhite((BitmapSource)ImageLeft.Source);
         }
 
         public BitmapSource Invert(BitmapSource source)
@@ -119,6 +121,46 @@ namespace FloydSteinbergDithering
                 null, data, stride);
         }
 
+
+        public BitmapSource BlackOrWhite(BitmapSource source)
+        {
+            // Calculate stride of source
+            int stride = (source.PixelWidth * source.Format.BitsPerPixel + 7) / 8;
+
+            // Create data array to hold source pixel data
+            int length = stride * source.PixelHeight;
+            byte[] data = new byte[length];
+
+            // Copy source image pixels to the data array
+            source.CopyPixels(data, stride, 0);
+
+            // MAKES Black or White !!!
+            for (int i = 0; i < length; i += 4)
+            {
+                // looks for average RGB number
+                int average = AverageNum(data[i], data[i + 1], data[i + 2]);
+
+                if(average < 128)
+                {
+                    data[i] = 0; //R
+                    data[i + 1] = 0; //G
+                    data[i + 2] = 0; //B
+                }
+                else
+                {
+                    data[i] = 255; //R
+                    data[i + 1] = 255; //G
+                    data[i + 2] = 255; //B
+                }
+            }
+
+            // Create a new BitmapSource from the inverted pixel buffer
+            return BitmapSource.Create(
+                source.PixelWidth, source.PixelHeight,
+                source.DpiX, source.DpiY, source.Format,
+                null, data, stride);
+        }
+
         void ChangePictureTEST() // Not Used
         {
             DrawingVisual drawVis = new DrawingVisual();
@@ -139,6 +181,15 @@ namespace FloydSteinbergDithering
             ImageRight.Source = rtb;
 
             ImageRight.Source = Invert((BitmapSource)ImageRight.Source);
+        }
+
+        int AverageNum(int one, int two, int three)
+        {
+            int[] listing = new int[] { one, two, three };
+
+            double result = listing.Average();
+
+            return (int)result;
         }
     }
 }
